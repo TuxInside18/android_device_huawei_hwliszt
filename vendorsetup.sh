12ac9a8e54496582b2fd3777366e314f39e48909
TuxInside18 <47487010+TuxInside18@users.noreply.github.com>
2023-01-25 19:27:38 +0100

    Switched to AOSP for Lollipop

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
-add_lunch_combo lineage_hwliszt-user
-add_lunch_combo lineage_hwliszt-userdebug
-add_lunch_combo lineage_hwliszt-eng
+add_lunch_combo aosp_hwliszt-user
+add_lunch_combo aosp_hwliszt-userdebug
+add_lunch_combo aosp_hwliszt-eng
